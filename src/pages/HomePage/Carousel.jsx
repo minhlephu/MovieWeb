23f93cfe4f03949b5da643d3226960f7fe55fabf
minhlephu <110329077+minhlephu@users.noreply.github.com>
2024-02-02 00:28:22 +0700

    slider

--- src/pages/HomePage/Carousel.jsx
+++ src/pages/HomePage/Carousel.jsx
@@ -1,16 +1,44 @@
-
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import homeCarouselData from "../../constrants/homeCarouselData";
 import Slider from "react-slick";
-// import { styled } from "@mui/system";
-function NextArrow() {
-    return <ArrowForwardIosIcon className="absolute  z-2 w-[50px] h-[100px]"/>;
-  }
-  
-  function PrevArrow() {
-    return <ArrowBackIosIcon className="absolute z-2 bottom-[50%]"  />;
-  }
+import PropTypes from "prop-types";
+import "./carousel.css";
+function NextArrow(props) {
+  const { onClick } = props;
+  return (
+    <ArrowForwardIosIcon
+      sx={{
+        height: 150,
+        width: 50,
+        transform: "translateY(-50%)",
+        color: "#d8d8d8 !important",
+        transition: "all .2s",
+        "&:hover": { color: "#fb4226 !important" },
+      }}
+      onClick={onClick}
+      className="absolute z-[2] top-[50%]  z-2 right-4 cursor-pointer"
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <ArrowBackIosIcon
+      sx={{
+        height: 150,
+        width: 50,
+        transform: "translateY(-50%)",
+        color: "#d8d8d8 !important",
+        transition: "all .2s",
+        "&:hover": { color: "#fb4226 !important" },
+      }}
+      onClick={onClick}
+      className="absolute z-[2] top-[50%] z-2 left-4 cursor-pointer"
+    />
+  );
+}
 const CarouselWeb = () => {
   const settings = {
     dots: true,
@@ -18,26 +46,40 @@ const CarouselWeb = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: true,
+    swipeToSlide: true,
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
+    dotsClass: "slickdotsbanner",
   };
+
   return (
-    <div className="relative">
-          <Slider {...settings}>
-              {homeCarouselData.map((banner) => {
-                  return (
-                      <div key={banner.maPhim} className="relative z-1">
-                          <img src={banner?.hinhAnh} alt="banner" className="w-full h-full"/>
-                          <div
-                              className="absolute w-full h-full bg-gradient-to-t from-black via-transparent to-transparent top-0"
-                              onClick={() => history.push(`/phim/${banner.maPhim}`)} />
-                          {/* <BtnPlay cssRoot={"play"} urlYoutube={banner.trailer} /> */}
-                      </div>
-                  );
-              })}
-          </Slider>
-      </div>    
+    <div className="relative z-0">
+      <Slider {...settings}>
+        {homeCarouselData.map((banner) => {
+          return (
+            <div key={banner.maPhim} className="relative z-1">
+              <img
+                src={banner?.hinhAnh}
+                alt="banner"
+                className="w-full h-full"
+              />
+              <div
+                className="absolute w-full h-full bg-gradient-to-t from-black via-transparent to-transparent top-0"
+                onClick={() => history.push(`/phim/${banner.maPhim}`)}
+              />
+              {/* <BtnPlay cssRoot={"play"} urlYoutube={banner.trailer} /> */}
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
   );
 };
-
+NextArrow.propTypes = {
+  onClick: PropTypes.func,
+};
+PrevArrow.propTypes = {
+  onClick: PropTypes.func,
+};
 export default CarouselWeb;

bbed2c1aea6c4214883de7944495c93efdc0bf93
minhlephu <110329077+minhlephu@users.noreply.github.com>
2024-02-27 13:10:32 +0700

    configlogin

--- src/redux/actions/AuthAction.js
+++ src/redux/actions/AuthAction.js
@@ -1,5 +1,6 @@
 import Swal from "sweetalert2";
 import { userService } from "../../services/userService";
+import { jwtDecode } from "jwt-decode";
 
 import { LOGIN_REQUEST, LOGIN_SUCCESS } from "../constrants/Auth";
 
@@ -11,7 +12,16 @@ export const signInAction = (user) => {
       });
       const result = await userService.signIn(user);
       if (result.data.code == 200) {
-        localStorage.setItem("user", JSON.stringify({ ...result.data.data }));
+        const decoded = jwtDecode(result.data.data);
+        const name =
+          decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"];
+        const role =
+          decoded[
+            "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+          ];
+        console.log("Name:", name);
+        console.log("Role:", role);
+        localStorage.setItem("user", result.data.data);
         dispatch({
           type: LOGIN_SUCCESS,
           payload: {
@@ -37,32 +47,28 @@ export const signInAction = (user) => {
     }
   };
 };
-// export const register = (user) => {
-//   return (dispatch) => {
-//     dispatch({
-//       type: REGISTER_REQUEST,
-//     });
-
-//     usersApi
-//       .postSignUp(user)
-//       .then((result) => {
-//         dispatch({
-//           type: REGISTER_SUCCESS,
-//           payload: {
-//             data: result.data,
-//           },
-//         });
-//       })
-//       .catch((error) => {
-//         dispatch({
-//           type: REGISTER_FAIL,
-//           payload: {
-//             error: error.response?.data ? error.response.data : error.message,
-//           },
-//         });
-//       });
-//   };
-// };
+export const signUpAction = (user) => {
+  return async ()=>{
+    try{
+      const result = await userService.signUp(user);
+      if(result.data.code==200){
+        Swal.fire({
+          title: "Đăng ký thành công!",
+          icon: "success",
+          confirmButtonText: "Đăng nhập ngay",
+        });
+      }
+    }
+    catch(error){
+      Swal.fire({
+        title: "Error!",
+        text: error.response.data.content,
+        icon: "error",
+        confirmButtonText: "Try Again!",
+      });
+    }
+  }
+};
 // export const resetErrorLoginRegister = () => {
 //   return (dispatch) => {
 //     dispatch({

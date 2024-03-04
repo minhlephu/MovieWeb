8562dd82e70322d6b9ca868ce97bff81cb5efb24
minhlephu <110329077+minhlephu@users.noreply.github.com>
2024-03-04 21:12:54 +0700

    commit logo

--- src/layouts/AdminLayout/TopBar/TopBar.jsx
+++ src/layouts/AdminLayout/TopBar/TopBar.jsx
@@ -1,51 +1,48 @@
 import {
-    AppBar,
-    Badge,
-    Box,
-    Hidden,
-    IconButton,
-    Toolbar,
-    Tooltip,
-  } from '@mui/material';
-import NotificationsIcon from '@mui/icons-material/NotificationsOutlined';
-import InputIcon from '@mui/icons-material/Input';
-import { useState } from 'react';
+  AppBar,
+  Badge,
+  Box,
+  Hidden,
+  IconButton,
+  Toolbar,
+  Tooltip,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/NotificationsOutlined";
+import InputIcon from "@mui/icons-material/Input";
+import { useState } from "react";
 const TopBar = () => {
   const [notifications] = useState([]);
 
-    return (
-        <AppBar
-        elevation={0}
-        position="static"
-      >
-        <Toolbar>
-          <div  style={{ cursor: "pointer" }}>
-            <img src="/img/headTixLogo.png" alt="logo" style={{ height: 50 }} />
-          </div>
-  
-          {/* 1 thẻ div chiếm hết khoảng trống còn lại dể dồn các icon về 2 bên */}
-          <Box flexGrow={1} />
-  
-          {/* cái icon chuông và đăng xuất */}
-          <Hidden lgDown>
+  return (
+    <AppBar elevation={0} position="static">
+      <Toolbar>
+        <div style={{ cursor: "pointer" }}>
+          <img src="/img/logo.png" alt="logo" style={{ height: 50 }} />
+        </div>
+
+        {/* 1 thẻ div chiếm hết khoảng trống còn lại dể dồn các icon về 2 bên */}
+        <Box flexGrow={1} />
+
+        {/* cái icon chuông và đăng xuất */}
+        <Hidden lgDown>
+          <IconButton color="inherit" size="large">
+            <Badge
+              badgeContent={notifications.length}
+              color="primary"
+              variant="dot"
+            >
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <Tooltip title="Đăng xuất">
             <IconButton color="inherit" size="large">
-              <Badge
-                badgeContent={notifications.length}
-                color="primary"
-                variant="dot"
-              >
-                <NotificationsIcon />
-              </Badge>
+              <InputIcon />
             </IconButton>
-            <Tooltip title="Đăng xuất">
-              <IconButton color="inherit" size="large">
-                <InputIcon />
-              </IconButton>
-            </Tooltip>
-          </Hidden>
-  
-          {/* cái icon menu */}
-          {/* <Hidden lgUp>
+          </Tooltip>
+        </Hidden>
+
+        {/* cái icon menu */}
+        {/* <Hidden lgUp>
             <IconButton
               color="inherit"
               // nếu click thì thực hiện func đóng mở Nav được truyền vào từ cha
@@ -54,9 +51,9 @@ const TopBar = () => {
               <MenuIcon />
             </IconButton>
           </Hidden> */}
-        </Toolbar>
-      </AppBar>
-    );
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default TopBar;

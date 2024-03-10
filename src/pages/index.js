992d32cae54b64f756dd9131da633055a5574dac
Nguyễn Văn Thọ <Nguyenthogr.th@gmail.com>
2024-03-11 00:50:13 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,3 @@
-
-export { default as DetailPage } from './DetailPage/DetailPage'
-export { default as HomePage } from './HomePage/HomePage'
 export { default as BookTicket } from './Bookticket/BookTicket'
 export { default as DetailPage } from "./DetailPage/DetailPage";
 export { default as HomePage } from "./HomePage/HomePage";
